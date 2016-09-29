6f4bcfeb194112fccdd5e81e332a10d09e98fb32
Joachim Barheine <joachim.barheine@sap.com>
2016-09-29 10:17:24 +0200

    prepare submission of prometheus plugin
    * clean-up code
    * remove now obsolete plugin for cAdvisor polling and deps

--- monasca_agent/collector/checks_d/prometheus.py
+++ monasca_agent/collector/checks_d/prometheus.py
@@ -11,9 +11,12 @@ import urlparse
 
 import math
 
+import requests
+
+# import prometheus client dependency dynamically
 try:
     import prometheus_client.parser as prometheus_client_parser
-except Exception:
+except ImportError:
     prometheus_client_parser = None
 
 # stdlib
@@ -21,18 +24,12 @@ import logging
 from datetime import datetime
 import calendar
 
-# 3rd party
-import urllib2
-
 # project
 import monasca_agent.collector.checks.utils as utils
 import monasca_agent.collector.checks.services_checks as services_checks
 import monasca_agent.common.exceptions as exceptions
 
-logging.basicConfig()
 log = logging.getLogger(__name__)
-
-PROMETHEUS_PREFIX = "prometheus"
 
 
 class Prometheus(services_checks.ServicesCheck):
@@ -52,8 +49,7 @@ class Prometheus(services_checks.ServicesCheck):
             if url.endswith('/federate'):
                 mapped_metrics = self._publisher.get_mapped_metrics(inst)
                 url += '?match[]={__name__=~"' + ("|".join(mapped_metrics) + '"')
-                filter = inst.get('match_labels')
-                for key, value in inst.get('match_labels', {}).iteritems():
+                for key, value in inst.get('match_labels', {}).items():
                     if isinstance(value, list):
                         url += ',{}=~"{}"'.format(key, "|".join(value))
                     else:
@@ -100,11 +96,12 @@ class Prometheus(services_checks.ServicesCheck):
     @staticmethod
     def _convert_timestamp(timestamp):
         # convert from string '2016-03-16T16:48:59.900524303Z' to a float monasca can handle 164859.900524
-        # conversion using strptime() works only for 6 digits in microseconds so the timestamp is limited to 26 characters
+        # conversion using strptime() works only for 6 digits in microseconds so the timestamp is limited to
+        # 26 characters
         ts = datetime.strptime(timestamp[:25] + timestamp[-1], "%Y-%m-%dT%H:%M:%S.%fZ")
         return calendar.timegm(datetime.timetuple(ts))
 
-    def _update_container_metrics(self, instance, metric_name, container, metric_type=None, timestamp=None,
+    def _update_container_metrics(self, instance, metric_name, container, timestamp=None,
                                   fixed_dimensions=None):
 
         # TBD determine metric from Prometheus input
@@ -115,7 +112,6 @@ class Prometheus(services_checks.ServicesCheck):
             self.log.debug('filtering out NaN value provided for metric %s{%s}', metric_name, labels)
             return
 
-
         self._publisher.push_metric(instance,
                                     metric=metric_name,
                                     value=value,
@@ -123,10 +119,12 @@ class Prometheus(services_checks.ServicesCheck):
                                     timestamp=timestamp,
                                     fixed_dimensions=fixed_dimensions)
 
-    def _retrieve_and_parse_metrics(self, url):
+    @staticmethod
+    def _retrieve_and_parse_metrics(url):
         """
         Metrics from prometheus come in plain text from the endpoint and therefore need to be parsed.
-        To do that the prometheus client's text_string_to_metric_families -method is used. That method returns a generator object.
+        To do that the prometheus client's text_string_to_metric_families -method is used. That method returns a
+        generator object.
 
         The method consumes the metrics from the endpoint:
             # HELP container_cpu_system_seconds_total Cumulative system cpu time consumed in seconds.
@@ -138,14 +136,15 @@ class Prometheus(services_checks.ServicesCheck):
             documentation -> e.g. ' container_cpu_system_seconds_total Cumulative system cpu time consumed in seconds. '
             type          -> e.g. ' counter '
             samples       -> e.g. ' [.. ,("container_cpu_system_seconds_total", {id="/",name="/"}, 1.59578817e+06),
-                                      ('container_cpu_system_seconds_total', {u'id': u'/docker', u'name': u'/docker'}, 922.66),
+                                      ('container_cpu_system_seconds_total', {u'id': u'/docker', u'name': u'/docker'},
+                                      922.66),
                                     ..] '
 
         :param url: the url of the prometheus metrics
         :return: metric_families generator
         """
-        str = urllib2.urlopen(url).read()
-        metric_families = prometheus_client_parser.text_string_to_metric_families(str)
+        body = requests.get(url).text
+        metric_families = prometheus_client_parser.text_string_to_metric_families(body)
         return metric_families
 
     def _update_metrics(self, instance):
@@ -155,9 +154,11 @@ class Prometheus(services_checks.ServicesCheck):
             raise Exception('No metrics retrieved cmd=%s' % self.metrics_cmd)
 
         for metric_family in metric_families_generator:
+            container = None
             try:
                 for container in metric_family.samples:
-                    self._update_container_metrics(instance, metric_family.name, container, metric_family.type)
+                    # currently there is no support for detecting metric types from P8S
+                    self._update_container_metrics(instance, metric_family.name, container)
             except Exception as e:
                 self.log.warning("Unable to collect metric: {0} for container: {1} . - {2} ".format(
                     metric_family.name, container[1].get('name'), repr(e)))
