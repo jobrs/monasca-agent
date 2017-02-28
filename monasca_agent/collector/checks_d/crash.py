3d8b8bf203d0b09b6ec08b2631be3dd7870258bc
Joachim Barheine <joachim.barheine@sap.com>
2017-02-28 11:50:02 +0100

    fix invalid value_meta of crash plugin

--- monasca_agent/collector/checks_d/crash.py
+++ monasca_agent/collector/checks_d/crash.py
@@ -13,8 +13,8 @@ log = logging.getLogger(__name__)
 
 class Crash(checks.AgentCheck):
 
-    def __init__(self, name, init_config, agent_config):
-        super(Crash, self).__init__(name, init_config, agent_config)
+    def __init__(self, name, init_config, agent_config, instances):
+        super(Crash, self).__init__(name, init_config, agent_config, instances=instances)
         self.crash_dir = self.init_config.get('crash_dir', '/var/crash')
         log.debug('crash dir: %s', self.crash_dir)
 
@@ -48,7 +48,7 @@ class Crash(checks.AgentCheck):
         """
         dimensions = self._set_dimensions(None, instance)
         dump_count = 0
-        value_meta = {'latest': datetime.now()}
+        value_meta = {'latest': unicode(datetime.now())}
 
         # Parse the crash directory
         if os.path.isdir(self.crash_dir):
